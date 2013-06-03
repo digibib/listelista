92481b720002e0a99da82f54bf2b871c97835484
Petter Goksøyr Åsen <boutrosboutrosboutros@gmail.com>
2013-06-03 14:28:11 +0200

    basic auth

--- listelista.rb
+++ listelista.rb
@@ -3,8 +3,14 @@ require "e"
 require "http"
 require "json"
 
+require_relative "settings.rb"
+
 class App < E
   map '/'
+
+  auth do |user, pass|
+    [user, pass] == [Settings::USER, Settings::PASSWORD]
+  end
 
   def index
     lists = Http.get("http://marc2rdf.deichman.no/api/users/mylists")
